dec0460df1aed4afa7b52b769a8d7638d3ddd854
Dpatel1998 <DPatel@academytrainee.com>
2021-06-11 17:12:34 +0000

    push

--- services-1/application/models.py
+++ services-1/application/models.py
@@ -4,4 +4,4 @@ class Soldier(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nationality = db.Column(db.String(30), nullable=False)
     player_class = db.Column(db.String(30), nullable=False)
-    build = db.Column(db.String(255), nullable=False)
+    build = db.Column(db.String(500), nullable=False)
