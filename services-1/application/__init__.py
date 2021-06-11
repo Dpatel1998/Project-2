830534277c5b8cb3faf20fc87a1891fe406eaf0c
Dpatel1998 <DPatel@academytrainee.com>
2021-06-11 21:48:55 +0000

    push

--- services-1/application/__init__.py
+++ services-1/application/__init__.py
@@ -1,12 +1,12 @@
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# import os
-# from os import getenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from os import getenv
 
-# # app = Flask(__name__)
+app = Flask(__name__)
 
-# # app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] ="sqlite:///data.db" #getenv("DATABASE_URI")
 
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-# from application import routes
+from application import routes
---
+++ services-1/application/routes.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+from sqlalchemy import desc
+import requests
+from application.models import Soldier
+from application import app, db
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
+def gen():
+    nationality_response = requests.get("http://services-2:5000/nationality").text
+    player_class_response = requests.get("http://services-3:5000/player_class").text
+    build_response = requests.post("http://services-4:5000/build", data = player_class_response).text
+    
+    view_Soldier = Soldier.query.order_by(Soldier.id.desc()).all()
+    
+    
+    new_build = Soldier(nationality = nationality_response, player_class = player_class_response, build = build_response)
+    db.session.add(new_build)
+    db.session.commit()
+
+    return render_template('index.html', nationality_response=nationality_response, player_class_response=player_class_response, build_response=build_response,view_players=view_soldier)
