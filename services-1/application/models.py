47b9abe0e883559dc8fe72885cf80a9409d7a770
Dpatel1998 <82821545+Dpatel1998@users.noreply.github.com>
2021-06-13 20:17:52 +0100

    Update models.py

--- services-1/application/models.py
+++ services-1/application/models.py
@@ -3,5 +3,5 @@ from application import db
 class Soldier(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nationality = db.Column(db.String(30),nullable=False)
-    soldier_class = db.Column(db.String(30),nullable=False)
+    player_class = db.Column(db.String(30),nullable=False)
     build = db.Column(db.String(500),nullable=False)
